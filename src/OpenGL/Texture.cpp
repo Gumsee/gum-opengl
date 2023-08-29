bf81e6c0c0165acf8cfc5205981c76d2371eea24
Gumse <gumse@gumse.de>
2023-08-29 16:42:50 +0200

    Di 29. Aug 16:42:50 CEST 2023

--- src/OpenGL/Texture.cpp
+++ src/OpenGL/Texture.cpp
@@ -6,8 +6,8 @@ const uint16_t Texture::Pixelformat::RGBA        = GL_RGBA;
 const uint16_t Texture::Pixelformat::RGB         = GL_RGB;
 const uint16_t Texture::Pixelformat::RG          = GL_RG;
 const uint16_t Texture::Pixelformat::R           = GL_R;
-const uint16_t Texture::Pixelformat::BGRA        = GL_BGRA;
-const uint16_t Texture::Pixelformat::BGR         = GL_BGR;
+//const uint16_t Texture::Pixelformat::BGRA        = GL_BGRA;
+//const uint16_t Texture::Pixelformat::BGR         = GL_BGR;
 const uint16_t Texture::Pixelformat::RGBA16F     = GL_RGBA16F;
 const uint16_t Texture::Pixelformat::RGBA32F     = GL_RGBA32F;
 
